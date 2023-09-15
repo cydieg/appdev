76474e853eaa505f95c49d83e8d49e408376c49e
MichaelMaliksi <cydiegargullo@gmail.com>
2023-09-15 23:44:13 +0800

    Task
    step 4

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,6 +4,11 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
+    public function test()
+    {
+        return view('main');
+    }
+
     public function index(): string
     {
         return view('welcome_message');
